8cfd37f8f4f5454b2e0e5f55862aba91a4da3ba3
Asa <asareel56@gmail.com>
2022-12-01 22:32:18 -0500

    first commit

---
+++ poa-backend/src/main/java/com/backend/poabackend/PoaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.poabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoaBackendApplication.class, args);
+    }
+
+}
